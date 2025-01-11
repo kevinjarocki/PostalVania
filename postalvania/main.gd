8d017bd02b5ebffa30df28f9883fc662efbb5dfc
DanielDomaracki <danieldomaracki@gmail.com>
2025-01-10 22:47:32 -0500

    swinging

--- postalvania/main.gd
+++ postalvania/main.gd
@@ -6,12 +6,12 @@ var complete = false
 func _process (delta):
 	if !complete:
 		time += delta
-		$Player2/Control/Timer.text = ("%.2f" %time + " sec")
+		$Player/Control/Timer.text = ("%.2f" %time + " sec")
 
 func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_ESCAPE:
-			$Player2.position = Vector2(0,0)
+			$Player.position = Vector2(0,0)
 
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	complete = true
