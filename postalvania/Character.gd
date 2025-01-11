eb35da3b44a44455fb8f42e641c71a14032d4345
DanielDomaracki <danieldomaracki@gmail.com>
2025-01-11 10:00:22 -0500

    dialogue box

--- postalvania/Character.gd
+++ postalvania/Character.gd
@@ -7,5 +7,4 @@ signal character_touched(first_touch)
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	character_touched.emit(first_touch)
 	first_touch = false
-	print ("here")
 	pass # Replace with function body.
--- postalvania/main.gd
+++ postalvania/main.gd
@@ -12,17 +12,22 @@ func _unhandled_input(event):
 	if event is InputEventKey:
 		if event.pressed and event.keycode == KEY_ESCAPE:
 			$Player.position = Vector2(0,0)
+
 		if event.pressed and event.keycode == KEY_P:
 			$Player.position = Vector2(0,0)
 			time = 0
 			complete = false
 
+		if event.pressed and event.keycode == KEY_SPACE and $Player/Control/NinePatchRect.visible:
+			$Player/Control/NinePatchRect.visible = false
+			print("hiding")
+
 func _on_area_2d_body_entered(body: Node2D) -> void:
 	complete = true
 	pass # Replace with function body.
 
-
 func _on_character_character_touched(first_touch: Variant) -> void:
 	if first_touch:
-		print ("deliver this package")
+		$Player/Control/NinePatchRect/Label.text = "Thank you for getting to me! Please deliver this package to the right! My friend the cat is waiting."
+		$Player/Control/NinePatchRect.visible = true
 	pass # Replace with function body.
