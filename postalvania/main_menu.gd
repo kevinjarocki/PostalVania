9cf46a657b74593d2e8b8ded7586a080d0191dd9
Kevin Jarocki <k_j11@hotmail.ca>
2025-01-18 00:02:06 -0500

    Added a sick cursor

--- postalvania/main_menu.gd
+++ postalvania/main_menu.gd
@@ -1,6 +1,11 @@
 extends Control
 
 
+
+
+var cursor = load("res://Assets/menu/cursor/cursor04_gr16x16.png")
+
+
 func _on_play_pressed():
 	await get_tree().create_timer(0.5).timeout
 	get_tree().change_scene_to_file("res://main.tscn")
@@ -15,3 +20,8 @@ func _on_quit_pressed():
 
 func _on_how_to_play_pressed():
 	await get_tree().create_timer(0.5).timeout
+
+
+func _on_ready() -> void:
+	Input.set_custom_mouse_cursor(cursor)
+	pass # Replace with function body.
