e17233654eec36c8d7f172409de15a4801a6805a
ForgedInCode <166935741+ForgedInCode@users.noreply.github.com>
2025-01-19 14:36:23 -0500

    spiderHelpr

---
+++ postalvania/spider_tutor.gd
@@ -0,0 +1,17 @@
+extends Node2D
+
+var direction = 1
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	rotate(direction*delta)
+	print(rotation)
+	if rotation > PI/2:
+		direction = -1
+		
+	elif rotation < -PI/2:
+		direction = 1
